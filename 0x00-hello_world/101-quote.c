8ea54d4468270bebccfeab368c10cf3b2bb52aa1
kahbeer42 <olabodex2m@gmail.com>
2022-12-09 12:27:07 -0800

    Positive

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+   #include <stdio.h>
+  #include <unistd.h>
+/**
+ * main - Entry
+ * Return: always 1
+*/
+int main(void)
+{	
+write(1, "and that piece of art is useful \" - Dora korpar, 2016-10-19\n", 59)         				          		  return (1);
+}
+
