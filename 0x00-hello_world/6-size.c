d4f65de33946efdd1b9af36a26dce486815d42e6
kahbeer42 <olabodex2m@gmail.com>
2022-12-08 22:12:14 -0800

    The size of data in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("Size of a char: %lubyte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %lubyte(s)\n", (unsigned long)sizeof(b));
-	Printf("Sizr of a lont int: %lubyte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a long long int: %lubyte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lubyte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	Printf("Size of a lont int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
