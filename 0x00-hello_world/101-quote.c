7896ec0dfa91a49eb31f5744a8c6953159bc4f1a
kahbeer42 <olabodex2m@gmail.com>
2022-12-10 08:28:13 -0800

    Quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,22 @@
-   #include <stdio.h>
-  #include <unistd.h>
-/**
- * main - Entry
- * Return: always 1
-*/
-int main(void)
-{	
-write(1, "and that piece of art is useful \" - Dora korpar, 2016-10-19\n", 59)         				          		  return (1);
-}
+/*
+ * File: 101-quote.c
+ *
+ * Auth: Brennan D baraban
+ */
 
+#include <stdio.h>
+
+/**
+ * main-Prints "and that piece of art is useful" - Dora Korpar,
+ *	2015-10-19, followed by a new line, to standard error.
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (1);
+}
