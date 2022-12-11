d92d73195fd9d6a058119c486e409308311d6a69
kahbeer42 <olabodex2m@gmail.com>
2022-12-11 01:20:58 -0800

    Combination of success

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,7 +14,7 @@ int main(void)
 	{
 		for (digit2 = digit1 + 1; digit2 < 9; digit++)
 		{
-			for (digit3 = digit2 + 1; digit3 < 10; digit++)
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
 				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
