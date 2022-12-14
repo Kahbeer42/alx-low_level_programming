620712ac2f5d45af995c985e8512283ec3004e79
kahbeer42 <olabodex2m@gmail.com>
2022-12-13 16:46:59 -0800

    learn your time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,22 +10,28 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
-
+		_putchar(',');
+		_putchar(' ');
 		for (mult = 1; mult <= 9; mult++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = num * mult;
-
-			if (prod <= 9)
-				_putchar(' ');
+			prod = (num * prod);
+			if ((prod / 10) > 0)
+			{
+				_putchar((prod / 10 + '0');
+			}
 			else
-				_putchar((prod / 10) + '0');
-
+			{
+				_putchar(' ');
+			}
 			_putchar((prod % 10) + '0');
-		}
-	}
 
-	_putchar('\n');
+			if (mult < 9)
+			{
+				-putchar(',');
+				_putchar(' ');
+			}
+
+		}
+		_putchar('\n');
+	}
 }
