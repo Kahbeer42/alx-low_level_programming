3317cd4abb353c4cae832d928eb99fcf8b6c4612
kahbeer42 <olabodex2m@gmail.com>
2022-12-14 01:54:18 -0800

    Nature made the natural number

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,11 +12,11 @@ int main(void)
 
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3) == 0i || (i % 5) == 0)
+		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
 	}
 
-	printf("%d'\n", sum);
+	printf("%d\n", sum);
 
 	return (0);
 }
