a51547830df47e5619b9c618c6f5fa0af2b0e964
kahbeer42 <olabodex2m@gmail.com>
2022-12-15 11:55:51 -0800

    ptint line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line in the terminal
+ * @n: number of times
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
