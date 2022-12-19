5b493712752f8950df56d6c9281040b20d6ff25d
kahbeer42 <olabodex2m@gmail.com>
2022-12-19 03:58:48 -0800

    message

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,7 +4,6 @@
  * print_rev - prints a string in reverse
  * @s: strings  to be print
  *
- * Return: 0
  */
 
 void print_rev(char *s)
@@ -13,12 +12,10 @@ void print_rev(char *s)
 
 	j = 0;
 	while (s[j] != '\0')
-	{
-		_putchar(s[j];
 		j++;
-	}
 
-	for (i = j - 1; 1 >= 0; i--)
+
+	for (i = j - 1; 1 > 0; i--)
 	{
 		_putchar(s[i]);
 	}
