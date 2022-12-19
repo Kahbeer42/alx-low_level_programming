ae7ce5ad547d3981e274ae4122e4da061cae9307
kahbeer42 <olabodex2m@gmail.com>
2022-12-19 01:24:20 -0800

    Stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two int
+ * @a: first int
+ * @b: second int
+ *
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *a;
+	*b = *a;
+	x = *b;
+}
