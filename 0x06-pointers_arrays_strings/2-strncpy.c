f79d1e3df633541c5cafb2f2b9c655e4dfb66788
kahbeer42 <olabodex2m@gmail.com>
2022-12-22 13:17:01 -0800

    message

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,11 @@
  *
  * Return: pointer to the string dest.
  */
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 
-	for (i = 0; i < n && *(src + i); i++)
+	for (i = 0; i < n && *(src + i) != '\0'; i++)
 	{
 		*(dest + i) = *(src + i);
 	}
