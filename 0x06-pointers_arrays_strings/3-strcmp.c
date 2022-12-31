f7446ba0fe5111eaf88279d56e97b4fda686d4c7
kahbeer42 <olabodex2m@gmail.com>
2022-12-31 08:31:02 -0800

    String compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,20 +10,18 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	}
+	int i;
 
-	if (*s1 == *s2)
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		return (0);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	i++;
 	}
+	return (0);
 
-	else
-	{
-		return (*s1 - *s2);
-	}
 }
 
