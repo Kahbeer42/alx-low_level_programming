10f0f85515a52a8565c47ddfdf21f0d3b57957a0
kahbeer42 <olabodex2m@gmail.com>
2022-12-31 07:46:08 -0800

    String copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,20 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+	int i;
 
-	for (i = 0; i < n && *(src + i) != '\0'; i++)
+	int i = 0;
+
+	while (i < n; &&src[i] != '\0')
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = src[i];
+		i++;
 	}
-	for (; i < n; i++)
+
+	while (i < n)
 	{
-	*(dest + i) = '\0';
+		dest[i] = '\0';
+		i++;
 	}
 	return (dest);
 }
