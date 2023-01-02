8624458e269949609926a99ad0fd11dd77495924
kahbeer42 <olabodex2m@gmail.com>
2023-01-02 10:17:54 -0800

    String char

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,13 @@ char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		if (*s == c)
-		{
+		if (*s != c)
+			s++;
+		else
 			return (s);
-		}
-		s++;
 	}
+	if (c == '\0')
+		return (s);
+
 	return (NULL);
 }
