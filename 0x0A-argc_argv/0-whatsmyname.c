ebfa6f7c8aeb802ed93ba57e97a908f44675757e
kahbeer42 <olabodex2m@gmail.com>
2023-01-06 09:35:22 -0800

    Argument name and count

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints it name, follow by new line
+ * @agrc: argument to count
+ * @argv: argument s
+ *
+ * Return: 0.
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * prints the number of arguments passed into it
+ * @argc: argument count
+ * @argv: argument s
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc -1);
+	return (0);
+}
