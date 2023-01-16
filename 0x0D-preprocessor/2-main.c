2aed0f906133e1db391d04ee590f9c6b243d1e75
kahbeer42 <olabodex2m@gmail.com>
2023-01-16 02:15:38 -0800

    header macro file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the name of the file it was
+ * compiled from, followed by a new line.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+
