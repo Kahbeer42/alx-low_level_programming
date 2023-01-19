78620b74531873352b3ccfde4b98c2eff4a5c071
kahbeer42 <olabodex2m@gmail.com>
2023-01-19 12:14:18 -0800

    variadic function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdio.h>
 
 /**
  * print_strings - function that prints strings
@@ -16,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 
 	if (separator == NULL)
-		separator = ("");
+		separator = " ";
 
 	for (i = 0; i < n; i++)
 	{
