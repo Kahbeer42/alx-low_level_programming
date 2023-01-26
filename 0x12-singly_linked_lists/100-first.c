a21ec0e98e8f843ed0b6c811411c03476ad65a39
kahbeer42 <olabodex2m@gmail.com>
2023-01-26 10:12:50 -0800

    Print hoberton wth 64 bit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * myStartupFun - Apply the constructor attribute to myStartupFun()
+ * so that it is executed before main()
+ */
+void myStartupFun(void) __attribute__ ((constructor));
+
+/**
+ * myStarupFun - implementation of myStartupFun
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
